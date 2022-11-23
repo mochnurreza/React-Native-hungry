932e6f274dfd0ed6be15d33ff214714a21bbbead
mochnurreza <nrreza2@gmail.com>
2022-11-23 19:48:06 +0700

    implementing global state to component

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,11 +1,19 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { useNavigation } from "@react-navigation/native";
+import {selectBasket, selectBasketTotal} from "../store/basket/Basketslice";
+import { useSelector } from "react-redux";
 
 export const Basket = () => {
+  const navigation = useNavigation()
+  const items = useSelector(selectBasket)
+  const basketTotal = useSelector(selectBasketTotal)
+
+  if (items.length === 0) return null;
   return (
     <View className="absolute bottom-10 w-full z-50">
-      <TouchableOpacity className="mx-5 bg-[#00CCBB] p-4 rounded-lg flex-row">
-        <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">Basket</Text>
+      <TouchableOpacity className="mx-5 bg-[#00CCBB] p-4 rounded-lg flex-row" onPress={() => navigation.navigate("Basket")}>
+        <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">{items.length}</Text>
         <Text className="flex-1 text-white font-extrabold tex-lg text-center">
             View Basket
         </Text>
--- src/screens/BasketScreen.jsx
+++ src/screens/BasketScreen.jsx
@@ -6,31 +6,62 @@ import {
   ScrollView,
   Image,
 } from "react-native";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { selectRestaurant } from "../store/restaurant/RestauranSlice";
+import {
+  removeFromBasket,
+  selectBasket,
+  selectBasketTotal,
+} from "../store/basket/Basketslice";
+import { urlFor } from "../sanity";
 
 export default function BasketScreen() {
+  const [groupItemsBasket, setGroupItemsBasket] = useState([])
+  const navigation = useNavigation()
+
+  const restaurant = useSelector(selectRestaurant)
+  const items = useSelector(selectBasket)
+  const basketTotal = useSelector(selectBasketTotal)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+   const groupItems = items.reduce((result, item) => {
+    (result[item.id] = result[item.id] || []).push(item);
+    return result;
+   });
+   setGroupItemsBasket(groupItems)
+  }, [items])
+  
   return (
     <SafeAreaView>
       <View className="flex-1 bg-gray-100">
         <View className="p-5 border-b border-[#00CCBB] bg-white shadow-xs">
           <View>
             <Text className="text-lg font-bold text-center">Basket</Text>
-            <Text className="text-center text-gray-400">Restauran title</Text>
+            <Text className="text-center text-gray-400">{restaurant.title}</Text>
           </View>
-          <TouchableOpacity className="rounded-full bg-gray-200 absolute top-3 right-5">
+          <TouchableOpacity className="rounded-full bg-gray-200 absolute top-3 right-5" onPress={navigation.goBack}>
             Icon
           </TouchableOpacity>
         </View>
         <ScrollView className="divide-y divide-gray-200">
+          {Object.entries(groupItemsBasket).map(([key, items]) => (
           <View className="flex-row items-center space-x-3 bg-white py-2 px-5">
-            <Text className="text-[#00CCBB]">item.length</Text>
-            <Image />
-            <Text className="flex-1">item name</Text>
+            <Text className="text-[#00CCBB]">{items.length}</Text>
+            <Image  source={{
+                  uri: urlFor(items[0]?.image).url(),
+                }}
+                className="h-12 w-12 rounded-full"/>
+            <Text className="flex-1">{items[0]?.name}</Text>
             <Text className="text-gray-600">price</Text>
             <TouchableOpacity>
-              <Text className="text-[#00CCBB] text-xs">Remove</Text>
+              <Text className="text-[#00CCBB] text-xs" onPress={() => dispatch(removeFromBasket({ id: key }))}>Remove</Text>
             </TouchableOpacity>
           </View>
+          ))}
         </ScrollView>
         <View className="p-5 bg-white mt-5 space-y-4">
           <View className="flex-row justify-between">
